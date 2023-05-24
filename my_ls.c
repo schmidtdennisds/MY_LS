c279476187e0636478b5b1f02d5fdeaa6ecc69a8
dennis schmidt <schmidt_d@qwasar.io>
2023-05-24 17:35:42 +0000

    Saving all the output in an array

--- my_ls.c
+++ my_ls.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <dirent.h>
 
+#define MAX_LEN 256
+
 int my_str_len(char* str) {
     int count = 0;
     while (str[count]) {
@@ -11,7 +13,7 @@ int my_str_len(char* str) {
     return count;
 }
 
-int my_str_cmp(char* str1, char* str2) {
+int my_str_eql(char* str1, char* str2) {
     if (my_str_len(str1) != my_str_len(str2)) {
         return 0;
     }
@@ -27,25 +29,37 @@ int my_str_cmp(char* str1, char* str2) {
 
 void setFlags(int ac, char** av, bool* flagA, bool* flagT) {
     if (ac == 2) {
-        if (my_str_cmp(av[1], "-a")) {
+        if (my_str_eql(av[1], "-a")) {
             *flagA = true;
-        } else if (my_str_cmp(av[1], "-t")) {
+        } else if (my_str_eql(av[1], "-t")) {
             *flagT = true;
         }
     } else if (ac == 3) {
         // checking for the first argument and setting flag
-        if (my_str_cmp(av[1], "-a")) {
+        if (my_str_eql(av[1], "-a")) {
             *flagA = true;
-        } else if (my_str_cmp(av[1], "-t")) {
+        } else if (my_str_eql(av[1], "-t")) {
             *flagT = true;
         }
 
         // checking for the second argument and setting flag
-        if (my_str_cmp(av[2], "-a") && !*flagA) {
+        if (my_str_eql(av[2], "-a") && !*flagA) {
             *flagA = true;
-        } else if (my_str_cmp(av[2], "-t") && !*flagT) {
+        } else if (my_str_eql(av[2], "-t") && !*flagT) {
             *flagT = true;
         }
+    }
+}
+
+void my_str_copy(char dest[MAX_LEN], char*src) {
+    for (int i = 0; i < MAX_LEN; i++) {
+        dest[i] = src[i];
+    }
+}
+
+void print_str_Array(char str_array[][MAX_LEN], int str_count) {
+    for (int i = 0; i < str_count; i++) {
+        printf("%s\n", str_array[i]);
     }
 }
 
@@ -54,15 +68,35 @@ int main(int ac, char** av) {
     bool flagT = false;
     struct dirent* entry;
     DIR* dir = opendir(".");
+    int strCount = 0;
 
     setFlags(ac, av, &flagA, &flagT);
     // printf("Flag A ist set: %d, Flag T is set: %d\n", flagA, flagT);
-    
+
+    // Sets the count of the all the strings in the output of my_ls
     while ((entry = readdir(dir)) != NULL) {
         char* name = entry->d_name;
         if (!flagA && name[0] == '.') {
             continue;
         }
-        printf("%s\n", entry->d_name);
+        strCount++;
     }
+    closedir(dir);
+
+    char str_array[strCount][MAX_LEN];
+
+    opendir(".");
+    int index = 0;
+    while ((entry = readdir(dir)) != NULL) {
+        char* name = entry->d_name;
+        if (!flagA && name[0] == '.') {
+            continue;
+        }
+        my_str_copy(str_array[index], name);
+        index++;
+    }
+    closedir(dir);
+
+    print_str_Array(str_array, strCount);
+    
 }
